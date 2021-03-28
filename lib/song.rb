46ea1cf6b195e5a83de8c3d84afd9c9d5c13ce77
Erica Little <eteney90@gmail.com>
2021-03-28 18:25:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,6 @@ attr_accessor :name, :artist, :genre
   @@count = 0
   @@genres = []
   @@artists = []
-  @@genre_count = {}
-  @@artist_count = {}
 
  def initialize(name, artist, genre)
    @name = name
@@ -30,6 +28,7 @@ attr_accessor :name, :artist, :genre
  end
 
  def self.genre_count
+     @@genre_count = {}
    @@genres.each do |genre|
      if genre_count[genre]
        genre_count[genre] += 1
@@ -41,6 +40,7 @@ attr_accessor :name, :artist, :genre
    end
 
  def self.artist_count
+     @@artist_count = {}
    @@artists.each do |artist|
      if artist_count[artist]
        artist_count[artist] += 1
