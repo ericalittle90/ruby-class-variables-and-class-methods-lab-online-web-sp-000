bd4aecf6ce3d481c6aa0ce6fdc9b79ca3c4fb5b2
Erica Little <eteney90@gmail.com>
2021-03-28 17:38:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,13 @@ attr_accessor :name, :artist, :genre
   @@genres = []
   @@artists = []
 
- def initialize
+ def initialize(name, artist, genre)
+   @name = name
+   @artist = artist
+   @genre = genre
    @@count += 1
+   @@genres << genre
+   @@artists << artist
  end
 
  def self.count
