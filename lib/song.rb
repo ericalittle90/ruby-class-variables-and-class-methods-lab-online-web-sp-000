26826738442105aa3672b7acad0c749c3ae2d8dd
Erica Little <eteney90@gmail.com>
2021-03-28 17:50:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,8 @@ attr_accessor :name, :artist, :genre
   @@count = 0
   @@genres = []
   @@artists = []
+  @@genre_count = {}
+  @@artist_count = {}
 
  def initialize(name, artist, genre)
    @name = name
@@ -26,5 +28,13 @@ attr_accessor :name, :artist, :genre
  def self.artists
    @@artists.uniq!
  end
+
+def self.genre_count
+
+  end
+
+def self.artist_count
+
+  end
 
 end
