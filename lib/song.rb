9931a052c6f83ac3b841073981a7bd6e4d6d07a5
Erica Little <eteney90@gmail.com>
2021-03-28 18:20:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,17 +29,25 @@ attr_accessor :name, :artist, :genre
    @@artists.uniq!
  end
 
- def self.genre_count(genre)
-   if
-     @@genres.include?(genre)
-     @@genre_count[genre] += 1
-   else
-     @@genre_count[genre] = 1
+ def self.genre_count
+   @@genre_count.each do |genre|
+     if genre_count[genre]
+       genre_count[genre] += 1
+     else
+       genre_count[genre] = 1
+     end
    end
-   @@genre_count
+   genre_count
    end
 
  def self.artist_count
-
+   @@artist_count.each do |artist|
+     if artist_count[artist]
+       artist_count[artist] += 1
+     else
+       artist_count[artist] = 1
+     end
+   end
+   artist_count
    end
 end
